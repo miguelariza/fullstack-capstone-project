a428b21d768b0856a96f9203af720345c994b623
miguelariza <miguelangel.ariza@gmail.com>
2026-08-04 11:26:03 -0400

    Added search results and range slider to search page

--- giftlink-frontend/src/components/SearchPage/SearchPage.js
+++ giftlink-frontend/src/components/SearchPage/SearchPage.js
@@ -78,12 +78,92 @@ function SearchPage() {
                         <h5>Filters</h5>
                         <div className="d-flex flex-column">
                             {/* Task 3: Dynamically generate category and condition dropdown options.*/}
+                            <div className="mb-3">
+                                <label className="form-label">Category</label>
+                                <select
+                                    className="form-select form-select-lg mb-3"
+                                    name="category"
+                                    id="category"
+                                    value={selectCategory}
+                                    onChange={(e) => setSelectCategory(e.target.value)}
+                                    required
+                                >
+                                    <option selected value="">All categories</option>
+                                    {categories.map((category, index) => (
+                                        <option key={index} value={category}>{category}</option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Condition</label>
+                                <select
+                                    className="form-select form-select-lg mb-3"
+                                    name="condition"
+                                    id="condition"
+                                    value={selectCondition}
+                                    onChange={(e) => setSelectCondition(e.target.value)}
+                                    required
+                                >
+                                    <option selected value="">All conditions</option>
+                                    {conditions.map((condition, index) => (
+                                        <option key={index} value={condition}>{condition}</option>
+                                    ))}
+                                </select>
+                            </div>
                             {/* Task 4: Implement an age range slider and display the selected value. */}
+                            <div className="mb-3">
+                                <label for="range1" className="form-label">Chronological Age</label>
+                                <input type="range" class="form-range" min="0" max="10" value="1" id="range4">
+                                Less than <output for="range1" id="rangeValue" aria-hidden="true"></output> years.
+                            </div>
                         </div>
                     </div>
                     {/* Task 7: Add text input field for search criteria*/}
                     {/* Task 8: Implement search button with onClick event to trigger search:*/}
                     {/*Task 5: Display search results and handle empty results with a message. */}
+                    {searchResults.length === 0 ? (
+                        <div className="mb-1">
+                            <p>No products match your criteria</p>
+                        </div>
+                    ) : (
+                        <div className="row">
+                            <h2 className="h3 fw-semibold text-dark">{searchResults.length} Search results</h2>
+                            {searchResults.map((gift) => (
+                            <div key={gift.id} className="col-md-4 mb-4">
+                                <div className="card product-card">
+                                    {/* // Task 4: Display gift image or placeholder */}
+                                    <div className="image-placeholder">
+                                        {
+                                            gift.image ? (
+                                                <img className="card-img-top" src={gift.image} alt={gift.name} />
+                                            ) : (
+                                                <div>No image available</div>
+                                            )
+                                        }
+                                    </div>
+                                    <div className="card-body">
+
+                                        {/* // Task 5: Display gift image or placeholder */}
+                                        {/* // Write your code below this line */}
+                                        <h5 className="card-title">{gift.name}</h5>
+
+                                        <p className={`card-text ${getConditionClass(gift.condition)}`}>
+                                        {gift.condition}
+                                        </p>
+
+                                        {/* // Task 6: Display gift image or placeholder */}
+                                        {/* // Write your code below this line */}
+                                        <p className="card-text">{formatDate(gift.date_added)}</p>
+
+                                        <button onClick={() => goToDetailsPage(gift.id)} className="btn btn-primary">
+                                            View Details
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
